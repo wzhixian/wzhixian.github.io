50010fc426785570edd9be27f76392c406c6e2b3
wangzhixian <2259654581@qq.com>
2019-11-26 14:19:09 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,3 +32,7 @@ jQuery(function() {
 
   toTop();
 });
+
+<script src="https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome/css/font-awesome.min.css"/>
+<script src="https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget/autoload.js"></script>
