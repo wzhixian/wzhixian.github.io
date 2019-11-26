e971aa22c336efb849f7a3b0f06befd47f723388
wangzhixian <2259654581@qq.com>
2019-11-26 14:31:10 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,6 +33,6 @@ jQuery(function() {
   toTop();
 });
 
-<script src="https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js"></script>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome/css/font-awesome.min.css"/>
-<script src="https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget/autoload.js"></script>
+document.write("<script src='https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js'></script>
+    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/font-awesome/css/font-awesome.min.css'/>
+    <script src='https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget/autoload.js'></script>");
